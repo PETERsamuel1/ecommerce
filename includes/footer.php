fed2f200b44980b843937e58e6544c15c943e0f0
Peter Samuel <petersamuelsam6@gmail.com>
2019-09-02 14:29:31 +0300

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<?php
+$cur_yr = date("Y");
+?>
+
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; <?php echo $cur_yr;?>&nbsp; <a href="http://localhost:8888/ecommerce/">Electronic Shop </a></strong>
+    </div>
+</footer>
+
